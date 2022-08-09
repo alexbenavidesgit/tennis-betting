825253cd8dca48b814e3671a3b4df2fb08e995cb
Alex Benavides <alexbenavidesep@gmail.com>
2022-08-09 11:57:47 -0500

    add algo

--- Python/live_draws.py
+++ Python/live_draws.py
@@ -1,22 +1,21 @@
-import requests
+import requests # pip install this
 
 url = "https://tennis-odds.p.rapidapi.com/odds/prematch"
 headers = {
-	"X-RapidAPI-Key": "b40773ca22msh5a915ad4a2798c4p12b84cjsn7c4517677c21",
+	"X-RapidAPI-Key": "b40773ca22msh5a915ad4a2798c4p12b84cjsn7c4517677c21", # Free key, gives us 500 calls per month. 20 per minute
 	"X-RapidAPI-Host": "tennis-odds.p.rapidapi.com"
 }
 newlist = []
 i = 0
 
 while i < 8: # TODO - fix this to be a real dynamic number instead of "8"
-    # This API only gives us 50 results at a time so we need to paginate
-    querystring = {"page":i,"size":"10"}
+    # This API only gives us the max (50) results at a time so we need to paginate
+    querystring = {"page":i,"size":"50"}
     response = requests.request("GET", url, headers=headers, params=querystring).json()
     i = i + 1
     # Filter out womens matches
     x = 0
     maxL = len(response)
-    print(maxL)
     while x < maxL:
         if "ATP" in response[x]['competition']['name']:
             newlist.append(response[x])
